8db31fd5212aaf7f60cda2b0e006953a54e4fe07
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-09-17 15:42:01 +0300

    create btn nav bar

---
+++ lib/features/botton_nav_bar/presentation/views/main_view.dart
@@ -0,0 +1,64 @@
+import 'package:durosy/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+import 'package:durosy/features/home/presentation/view/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MainView extends StatelessWidget {
+  MainView({super.key});
+
+  final List pages = [HomeView(), HomeView()];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BottomNavCubit, BottomNavState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+
+          body: Stack(
+            children: [
+              pages[state.currentIndex],
+
+              Positioned(
+                bottom: 16,
+                left: 16,
+                right: 16,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: BottomNavigationBar(
+                      backgroundColor: Colors.white,
+                      currentIndex: state.currentIndex,
+                      onTap: context.read<BottomNavCubit>().changeTab,
+
+                      showSelectedLabels: false,
+                      showUnselectedLabels: false,
+                      type: BottomNavigationBarType.fixed,
+
+                      selectedItemColor: Color(0xFF1A1A1A),
+                      unselectedItemColor: Color(0xFF6C6C6C),
+
+                      items: const [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.person_outline),
+                          label: '',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.post_add_outlined),
+                          label: '',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:durosy/features/home/presentation/view/home_view.dart';
-import 'package:flutter/material.dart';
+import 'package:durosy/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+import 'package:durosy/features/botton_nav_bar/presentation/views/main_view.dart';
+ import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const Durosy());
@@ -10,6 +13,29 @@ class Durosy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeView());
+    return BlocProvider(
+      create: (context) => BottomNavCubit(),
+      child: MaterialApp(
+        locale: const Locale('ar'),
+
+        supportedLocales: const [Locale('ar')],
+
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+
+        builder: (context, child) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: child!,
+          );
+        },
+
+        debugShowCheckedModeBanner: false,
+        home: MainView(),
+      ),
+    );
   }
 }
